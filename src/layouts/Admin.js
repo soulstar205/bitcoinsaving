eb4a1628ee7f7ca19a935144e4ae296658a6b4fb
soulstar205 <soulstar205@gmail.com>
2023-01-22 15:18:48 +0100

    not maps

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -10,7 +10,7 @@ import FooterAdmin from "./../components/Footers/FooterAdmin";
 // views
 
 import Dashboard from "./../views/admin/Dashboard.js";
-import Maps from "./../views/admin/Maps.js";
+// import Maps from "./../views/admin/Maps.js";
 import Settings from "./../views/admin/Settings.js";
 import Tables from "./../views/admin/Tables.js";
 
@@ -25,7 +25,7 @@ export default function Admin() {
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
           <Switch>
             <Route path="/admin/dashboard" exact component={Dashboard} />
-            <Route path="/admin/maps" exact component={Maps} />
+            {/* <Route path="/admin/maps" exact component={Maps} /> */}
             <Route path="/admin/settings" exact component={Settings} />
             <Route path="/admin/tables" exact component={Tables} />
             <Redirect from="/admin" to="/admin/dashboard" />
